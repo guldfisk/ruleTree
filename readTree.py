5963aed6f198f2663c0fa229d4f7947562a69517
guldfisk <ce.guldfisk@gmail.com>
2016-11-25 20:21:40 +0100

    hot

---
+++ readTree.py
@@ -0,0 +1,30 @@
+from ruleTree import *
+import sys
+import re
+
+def reads(s):
+	root = Tree(seperator='')
+	current = [root]
+	for m in re.finditer('(\w*)(?<!\\\\)(<+|\||>+)(.+?)\s*(?<!\\\\)(?=\w*(?:<+|\||>+|.\Z))', t, re.DOTALL+re.UNICODE):
+		label, action, content = m.groups()
+		move = len(action)
+		tree = Tree(content, label)
+		if action[0]=='>' and current[-1].branches:
+			current[-1].branches[-1].addBranch(tree)
+			current.append(current[-1].branches[-1])
+		elif action[0]=='<' and len(current)>move and current[-move].branches:
+			current = current[:-move]
+			current[-1].addBranch(tree)
+		else: current[-1].addBranch(tree)
+	return root
+		
+if __name__=='__main__' and len(sys.argv)>1:
+	file = open(sys.argv[1], 'rb')
+	t = file.read().decode('UTF-8')
+	file.close()
+	root = reads(t)
+	if len(sys.argv)>2: path = sys.argv[2]
+	else: path = re.match('(.+)(\..*?\Z)', sys.argv[1]).groups()[0]+'.txt'
+	file = open(path, 'wb')
+	file.write(root.dumps().encode('UTF-8'))
+	file.close()
